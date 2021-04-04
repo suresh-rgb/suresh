2fc2543528ae18f5bca7cea5082aa0443eaaaec6
sures <sures@LAPTOP-954R2NVB>
2021-04-04 12:08:43 +0530

    added line

--- git1/src/git1/Launch1.java
+++ git1/src/git1/Launch1.java
@@ -4,6 +4,7 @@ public class Launch1 {
 
 	public static void main(String[] args) {
                System.out.println("hello Github welcome to the world");
+               System.out.println("Hello java");
 	}
 
 }
