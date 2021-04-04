fc5ea554bc6e8c83688346572300120033bb733f
THOTA <THOTA@THOTA>
2021-04-04 13:12:19 +0530

    ok

--- git1/src/git1/Launch1.java
+++ git1/src/git1/Launch1.java
@@ -5,6 +5,7 @@ public class Launch1 {
 	public static void main(String[] args) {
                System.out.println("hello Github welcome to the world");
                System.out.println("Hello java");
+               System.out.println("hello.");
 	}
 
 }
